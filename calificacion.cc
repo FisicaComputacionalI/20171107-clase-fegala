eb812d1da7a059cab4d7125352405d5be1b3aa14
fegala <31254488+fegala@users.noreply.github.com>
2017-11-07 14:10:06 -0600

    Add files via upload
    Archivo calificacion no terminado aun

---
+++ calificacion.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float c;
+  cout<<"Calificaciòn del examen "<<endl; cin>>c;
+  {  if (c< 8)
+      {float t=3;
+	float p=4;
+	float m=8; 
+      cout<<"Calificacion tarea "<<endl; cin>>t;
+      cout<<"Calificacion participaciòn "<<endl; cin>>p;
+         c=c*.8; t=t*.15; p=p*.15; m=t+p+c;
+      cout << c << " " << t <<" " << p << endl;
+      cout<<"Tu calificacion final es "<<m <<endl;
+}
+  }
+  {  if (c>= 8)
+      float t=3;
+    float p=4;
+    float m=8;
+      { if (p=0;t=0)
+	cout<<"Tarea "<<endl; cin>>t;
+    cout<<"Participacion "<<endl; cin>>p;
+}
+  return 0;
+}
